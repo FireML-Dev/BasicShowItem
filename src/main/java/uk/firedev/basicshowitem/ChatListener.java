cf0dedcdf0c446cc61f6db8973e921ef01035e32
FireML <fireml@firedev.uk>
2026-03-14 14:28:51 +0000

    Fix "chat.square_brackets" issue
    Closes #2

--- src/main/java/uk/firedev/basicshowitem/ChatListener.java
+++ src/main/java/uk/firedev/basicshowitem/ChatListener.java
@@ -5,6 +5,8 @@ import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
 public class ChatListener implements Listener {
 
@@ -14,11 +16,32 @@ public class ChatListener implements Listener {
             return;
         }
         ItemStack item = event.getPlayer().getInventory().getItemInMainHand();
-        Component hover = item.displayName();
+        Component hover = buildHover(item);
         Component message = event.message()
             .replaceText(builder -> builder.matchLiteral("[i]").replacement(hover))
             .replaceText(builder -> builder.matchLiteral("[item]").replacement(hover));
         event.message(message);
+    }
+
+    private Component buildHover(@NotNull ItemStack item) {
+        if (item.isEmpty()) {
+            return Component.text("[Air]");
+        }
+        return Component.text()
+            .append(Component.text("["))
+            .append(retrieveDisplayName(item))
+            .append(Component.text("]"))
+            .hoverEvent(item)
+            .build();
+    }
+
+    private Component retrieveDisplayName(@NotNull ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) {
+            return Component.text("Air");
+        }
+        Component display = meta.displayName();
+        return display == null ? Component.translatable(item) : display;
     }
 
 }
